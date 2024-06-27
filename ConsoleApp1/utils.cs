6fa9cfe81c2bd61f2014e660bae3dd021226e892
Albin D Mamachen <142232645+Khalimovgeek@users.noreply.github.com>
2024-06-27 17:50:31 +0530

    Add project files.

---
+++ ConsoleApp1/utils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class utils
+    {
+        public static void link(string url)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(url);
+            }
+            catch (System.ComponentModel.Win32Exception nointernet)
+            {
+                if (nointernet.ErrorCode == 404)
+                {
+                    Console.WriteLine("No internet");
+                }
+            }
+
+        }
+    }
+    
+}
