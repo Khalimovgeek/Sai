3e6ad6dfdbd13f76c8e55cbf75df7e7aa1ce0910
Albin D Mamachen <142232645+Khalimovgeek@users.noreply.github.com>
2024-06-30 16:02:58 +0530

    some more edits

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace ConsoleApp1
 {
@@ -12,23 +13,35 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            var x = Console.ReadLine();  // Replace with Console.ReadLine() for actual input
-            var first = x.Split(' ')[0].ToLower();
-            var cmd = x.Substring(first.Length).Trim();
-
-            if (first == "sai")
+            while (true)
             {
-                if (cmd == "install") { Console.WriteLine("Loading"); }
-                else if (cmd == "detail")
+                string price, size, Dldfrm, cat;
+                price=size=Dldfrm=cat= "unknown";
+                var x = Console.ReadLine();  // Replace with Console.ReadLine() for actual input
+                var first= x.Split(' ')[0].ToLower();
+                var cmd = x.Substring(first.Length).Trim();
+                var cmd1 =cmd.Split(' ').First();
+                var app = cmd.Split(' ').Last();
+                if (first == "sai" && cmd1 !=app)
                 {
-                    Console.WriteLine($"General Details:\n Price: /free or paid/\n Size: /size of app/ \nDownloading from: /url or the app for downloading/ \nCategory: /type of the app/");
+                    switch (cmd1) 
+                    {
+                        case "install":
+                            Console.WriteLine("Loading");
+                            break;
+                        case "detail":
+                            Console.WriteLine($"General Details:\n Name:{app}\nPrice: {price}\n Size:{size} \nDownloading from:{Dldfrm} \nCategory:{cat}");
+                            break;
+
+                        default:
+                            Console.WriteLine("Commands:\ninstall: install a package\ndetail: gives a description about an application");
+                            break;
+                    }
                 }
-                else
+                else if (first =="sai" && cmd1 == app)
                 {
                     Console.WriteLine("Commands:\ninstall: install a package\ndetail: gives a description about an application");
                 }
-                
-
             }
         }
     }
