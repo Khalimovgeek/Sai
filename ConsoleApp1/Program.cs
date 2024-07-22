1cc919a7181c0f4bd88ee9c2c942fce8671095ed
Albin D Mamachen <142232645+Khalimovgeek@users.noreply.github.com>
2024-07-22 18:26:16 +0530

    concept_design

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.Azure.CognitiveServices.Search.WebSearch;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Schema;
@@ -15,22 +18,21 @@ namespace ConsoleApp1
         {
             while (true)
             {
-                string price, size, Dldfrm, cat;
-                price=size=Dldfrm=cat= "unknown";
+                
                 var x = Console.ReadLine();  // Replace with Console.ReadLine() for actual input
-                var first= x.Split(' ')[0].ToLower();
+                var first = x.Split(' ')[0].ToLower();
                 var cmd = x.Substring(first.Length).Trim();
-                var cmd1 =cmd.Split(' ').First();
+                var cmd1 = cmd.Split(' ').First();
                 var app = cmd.Split(' ').Last();
-                if (first == "sai" && cmd1 !=app)
+                if (first == "sai" && cmd1 != app)
                 {
-                    switch (cmd1) 
+                    switch (cmd1)
                     {
                         case "install":
-                            Console.WriteLine("Loading");
+                            ins(app);
                             break;
-                        case "detail":
-                            Console.WriteLine($"General Details:\n Name:{app}\nPrice: {price}\n Size:{size} \nDownloading from:{Dldfrm} \nCategory:{cat}");
+                        case "search":
+                            sr(app);
                             break;
 
                         default:
@@ -38,11 +40,28 @@ namespace ConsoleApp1
                             break;
                     }
                 }
-                else if (first =="sai" && cmd1 == app)
+                else if (first == "sai" && cmd1 == app)
                 {
                     Console.WriteLine("Commands:\ninstall: install a package\ndetail: gives a description about an application");
                 }
             }
+
         }
-    }
+        public static async void ins(string app)
+        {
+            Console.WriteLine($"Searching {app}");
+            await Task.Delay(1000);
+            Console.WriteLine("Preparing Installation");
+            Console.WriteLine($"{app} installed");
+        }
+        public static async void sr(string app) 
+        {
+            string price, size, Dldfrm, cat;
+            price = size = Dldfrm = cat = "unknown";
+            Console.WriteLine($"Searching {app}");
+            await Task.Delay(1000);            
+            Console.WriteLine($"General Details:\n Name:{app}\nPrice: {price}\n Size:{size} \nDownloading from:{Dldfrm} \nCategory:{cat}");
+
+        }
+     }
 }
